8fe414246a5f36d9e148e6ebf70695c2cbba6cc2
prashant_falnikar@310322 <prashant.falnikar@einzigtech.com>
2022-05-31 11:31:30 +0530

    first commit mailer

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailerController extends AbstractController
+{
+    #[Route('/email')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        $email = (new Email())
+            ->from('pfalnikarp@gmail.com')
+            ->to('pfalnikarp@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p><span>Second email</span>');
+
+        $mailer->send($email);
+
+        return $this->redirectToRoute('/email');
+
+
+    }
+}
